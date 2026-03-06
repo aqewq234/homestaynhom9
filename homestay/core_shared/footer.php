f44c53f7a33c619b14ce4b867f34827ede3e514c
aqewq234 <trantrunghieu290505@gmail.com>
2026-03-06 23:01:16 +0700

    Delete homestay directory

--- homestay/core_shared/footer.php
+++
@@ -1,6 +0,0 @@
-</main> <footer class="footer">
-        <p>&copy; 2026 Hệ thống quản lý Homestay.</p>
-        <p><a href="../task9/admin_panel.php" style="color: #aaa; text-decoration: none;">Vào trang Admin (T9)</a></p>
-    </footer>
-</body>
-</html>
